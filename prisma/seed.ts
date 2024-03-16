3e94efd0e177a7f170bf2767d295aa59aa4c90c9
Saadat Ali <saadataliklasra33@gmail.com>
2024-03-16 17:54:54 +0500

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,13 +2,24 @@ import { prisma } from "../src/lib/db"
 import categories from "./uniqueCategories"
 
 async function main() {
-  const response = await prisma.category.createMany({
-    data: [
-      ...categories.map((category) => ({ name: category.toLowerCase() })),
-    ],
-    skipDuplicates: true, // Skip 'duplicates' (if any) and continue with the next operation
-  })
-  console.log(response)
+  // const response = await prisma.category.createMany({
+  //   data: [
+  //     ...categories.map((category) => ({ name: category })),
+  //   ],
+  //   skipDuplicates: true, // Skip 'duplicates' (if any) and continue with the next operation
+  // })
+  for (const category of categories) {
+    const response = await prisma.category.update({
+      where: {
+        name: category.toLowerCase()
+      },
+      data: {
+        name: category
+      }
+    })
+    console.log(response)
+  }
+
 }
 main()
   .then(async () => {
